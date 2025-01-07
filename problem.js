e07358aa68209f1cbff32855b6bc477ee8d04db1
Hosain <cryptichasan925@gmail.com>
2025-01-07 23:22:31 +0600

    problem 3 solved

--- problem.js
+++ problem.js
@@ -40,4 +40,27 @@ function getBookTitles(bookArray) {
 const bookTitles = getBookTitles(books);
 
 // Print the result
-console.log(bookTitles);
+// console.log(bookTitles);
+
+
+// 4. Task: Sorting Objects
+
+// Create an array of objects representing cars with properties like make, model, and year. Write a function to sort the array of cars by the year of manufacture in ascending order. Print the sorted array.
+
+// Array of car objects
+const cars = [
+    { make: "Toyota", model: "Corolla", year: 2005 },
+    { make: "Honda", model: "Civic", year: 2010 },
+    { make: "Ford", model: "Mustang", year: 2000 },
+    { make: "Tesla", model: "Model 3", year: 2018 }
+  ];
+   
+  function sortCarsByYear(carArray) {
+    return carArray.sort((a, b) => a.year - b.year);
+  }
+   
+  const sortedCars = sortCarsByYear(cars);
+  
+  // Print the sorted array
+  console.log(sortedCars);
+  
